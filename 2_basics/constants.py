c719c479d2201b10ec7ba5df08e971867e07a073
Luis Ceballos <ceballos97@gmail.com>
2025-12-10 12:39:46 -0800

    Worked on some files...

---
+++ 2_basics/constants.py
@@ -0,0 +1,4 @@
+# Theres no proper way for defining constants. Best practice is to keep the varibale name capitalized.
+PI: float = 3.14
+API_KEY: str = "cnksdjvf3pirjneekdj24u723098"
+
---
+++ 2_basics/variables.py
@@ -0,0 +1,12 @@
+greeting: str = "Hello"
+good_feeling: str = "Happy"
+
+# print(f"{greeting} Luis")
+
+
+# Both work becuase each X has a completley different uni-code
+x: int = 1
+X: int = 2
+
+print(x)
+print(X)
