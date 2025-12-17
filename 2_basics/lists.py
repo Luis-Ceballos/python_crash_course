bfd4a35f5d45190cec5b730cf4d32adfdfb764ed
Luis Ceballos <ceballos97@gmail.com>
2025-12-16 17:31:36 -0800

    Finished lists

--- 2_basics/lists.py
+++ 2_basics/lists.py
@@ -8,3 +8,17 @@ print(drivers[-1])  # A negative index will start from the end -> 'Carlos'
 print(drivers[0:2])  # Returns a range -> Lewis, Lando, Max
 
 print("Max" in drivers)  # Will return True
+
+drivers[0] = "Checo" # This will replace value at index 0 with new value -> 'Checo'
+print(drivers[0])
+
+drivers.insert(4, '!!!') # Will insert '!!!' at the 4th inded
+print(drivers)
+drivers.append('Valteri') # Append adds the value to the end of the list
+
+drivers.pop(4) # Removes the value at the 4th index
+print(drivers)
+
+reserve_drivers: list[str] = ['Oscar', 'Charles', 'kimmi']
+drivers.extend(reserve_drivers) # Will return a concatinated list
+print(drivers)
